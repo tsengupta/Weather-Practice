82013f2d59827e32d52707d9ed2b39c45a623114
tsengupta <tsengupta64@gmail.com>
2015-06-30 16:08:56 -0500

    added days of the week for weather forecasting

---
+++ Weather-Practice/main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    var weatherData = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Nashville&mode=json&units=metric&cnt=7';
+
+    $.get(weatherData, function (data) {
+        console.log(data);
+        for (var i = 0; i < data.list.length; i++) {
+            var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            $("#weather").append("The weather for " + day[i] + " is " + data.list[i].weather[0].description + ". ");
+        }
+    }
+)});
