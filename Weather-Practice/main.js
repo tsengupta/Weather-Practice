132bed5183ad0840db0bb58f826cd37c655266f4
tsengupta <tsengupta64@gmail.com>
2015-07-02 02:53:57 -0500

    made weatherDictionary for own descriptions of forecast

--- Weather-Practice/main.js
+++ Weather-Practice/main.js
@@ -1,30 +1,100 @@
 ﻿$(document).ready(function () {
-    var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=Nashville,us';
+    var weatherURL = 'http://api.openweathermap.org/data/2.5/find?q=Nashville&units=imperial';
 
     $.get(weatherURL, function (data) {
         console.log(data);
-        var weatherCondition = data.weather[0].id;
+        var weatherCondition = data.list[0].weather[0].id;
+        var weatherDictionary = {
+            // Thunderstorms
+            200: "Thunderstorm with Light Rain",
+            201: "Thunderstorm with Rain",
+            202: "Thunderstorm with Heavy Rain",
+            210: "Light Thunderstorm",
+            211: "Thunderstorm",
+            212: "Heavy Thunderstorm",
+            221: "Ragged Thunderstorm",
+            230: "Thunderstorm with Light Drizzle",
+            231: "Thunderstorm with Drizzle",
+            232: "Thunderstorm with Heavy Drizzle",
 
-        if (weatherCondition < 233) {
-            $("#weather-id").text("Thunderstorm");
-        }
-        else if (weatherCondition < 322) {
-            $("#weather-id").text("Drizzle");
-        }
-        else if (weatherCondition < 532) {
-            $("#weather-id").text("Rain");
-        }
-        else if (weatherCondition < 623) {
-            $("#weather-id").text("Snow");
-        }
-        else if (weatherCondition < 782) {
-            $("#weather-id").text("Atmospheric Covering");
-        }
-        else if (weatherCondition < 805) {
-            $("#weather-id").text("Cloudy");
-        }
+            // Drizzles
+            300: "Light Drizzle",
+            301: "Drizzle",
+            302: "Moderate Drizzle",
+            310: "Quite the Drizzle",
+            311: "So Much Drizzle, it's Almost Rain!",
+            312: "Heavy Drizzle",
+            313: "Drizzle Intensifies",
+            314: "Drizzling at Maximum Capacitiy",
+            321: "A Showering of Drizzleness",
+
+            //Rain
+            500: "Light Rain",
+            501: "Moderate Rain",
+            502: "Quite a Lot of Rain",
+            503: "So Much Rain Everywhere",
+            504: "EXTREME RAIN",
+            511: "Freezing Rain",
+            520: "Rain Intensifies",
+            521: "Rain Shower",
+            522: "Rain Extremely Intensifies",
+            531: "Ragged Rain Shower",
+
+            //Snow
+            600: "Light Snowfall",
+            601: "Snow!",
+            602: "Heavy Snowfall",
+            611: "Sleet",
+            612: "Sleet Shower",
+            615: "Light Rain and Snow",
+            616: "Rain and Snow",
+            620: "Light Snow Shower",
+            621: "Snow Shower",
+            622: "Heavy Snow Shower",
+
+            // Atmosphere
+            701: "Mist",
+            711: "Smoke",
+            721: "Haze",
+            731: "Sand, Dust Whirls",
+            741: "Fog",
+            751: "Sand",
+            761: "Dust",
+            762: "Volcanic Ash",
+            771: "Squalls",
+            781: "Tornado",
+
+            // Clouds
+            800: "Clear Sky",
+            801: "A Few Clouds",
+            802: "Scattered Clouds",
+            803: "Broken Clouds",
+            804: "Overcast Clouds",
+
+            // Extreme
+            900: "Tornado",
+            901: "Tropical Storm",
+            902: "Hurricane",
+            903: "Super Duper Cold",
+            904: "Super Duper Hot",
+            905: "Terribly Windy",
+            906: "Hail on Earth",
+
+            // Additional
+            951: "Calm Day",
+            952: "A Pleasant Light Breeze",
+            953: "A Delightfully Gentle Breeze",
+            954: "A Satisfying Moderate Breeze",
+            955: "A Fresh, Enriching Breeze",
+            956: "An Agitated Strong Breeze",
+            957: "High Wind, Near Gale",
+            958: "Gale",
+            959: "Severe Gale",
+            960: "Storm",
+            961: "Quite the Harsh and Violent Storm",
+            962: "Hurricane"
+        };
+        var wString = weatherDictionary[weatherCondition];
+        $("#weather-forecast").text(wString);
     });
-    
-
-
 });
